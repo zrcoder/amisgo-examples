a7bc46805844ec71189acab934af451a9f127481
zhangrangding <love-nankai@163.com>
2025-03-03 17:08:38 +0800

    improve todo-app's ui

--- todo-app/ui/util.go
+++ todo-app/ui/util.go
@@ -4,6 +4,6 @@ import "github.com/zrcoder/amisgo/comp"
 
 func (u *UI) page(body ...any) comp.Page {
 	return u.Page().UseMobileUI(false).ClassName("p-8").Title(
-		u.Button().Label("${i18n.name}").ActionType("link").Link("/").ClassName("font-bold bg-none border-none"),
+		u.Button().Icon("fa fa-tasks").Label("${i18n.name}").ActionType("link").Link("/").ClassName("font-bold bg-none border-none"),
 	).Toolbar(u.DrawerAction().Label("${i18n.settings}").Icon("fa fa-cog").Drawer(u.setting())).Body(body...)
 }
